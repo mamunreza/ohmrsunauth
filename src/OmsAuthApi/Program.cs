23a8de2ecea238ff6961d7a00d2dd8e2c35bf942
Mamun Reza <Muhammad.Reza@energia.ee>
2024-07-25 11:25:27 +0300

    OMS-5 Some cleanups

--- src/OmsAuthApi/Program.cs
+++ src/OmsAuthApi/Program.cs
@@ -93,11 +93,14 @@ namespace OmsAuthApi
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            //if (app.Environment.IsDevelopment())
+            //{
+            //    app.UseSwagger();
+            //    app.UseSwaggerUI();
+            //}
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
             app.MapIdentityApi<IdentityUser>();
 
