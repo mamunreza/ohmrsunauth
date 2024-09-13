5f3c253894498ccaf9d036fa85c0e5594005581c
Mamun Reza <mamun.reza09@gmail.com>
2024-09-13 16:19:49 +0300

    OMS-21 User registration added (#11)
    Co-authored-by: Mamun Reza <Muhammad.Reza@energia.ee>

--- src/OmsAuthApi/Controllers/AuthController.cs
+++ src/OmsAuthApi/Controllers/AuthController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.Data;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
@@ -12,11 +13,32 @@ namespace OmsAuthApi.Controllers;
 public class AuthController : ControllerBase
 {
     private readonly IConfiguration _configuration;
+    private readonly UserManager<IdentityUser> _userManager;
+    private readonly SignInManager<IdentityUser> _signInManager;
 
-    public AuthController(IConfiguration configuration)
+    public AuthController(IConfiguration configuration, UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
     {
         _configuration = configuration;
+        _userManager = userManager;
+        _signInManager = signInManager;
     }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromBody] RegisterRequest request)
+    {
+        var user = new IdentityUser { UserName = request.Email, Email = request.Email };
+        var result = await _userManager.CreateAsync(user, request.Password);
+
+        if (result.Succeeded)
+        {
+            var token = GenerateJwtToken(request.Email);
+
+            return Ok(new { token });
+        }
+
+        return BadRequest(result.Errors);
+    }
+
 
     [HttpPost("login")]
     public IActionResult Login([FromBody] LoginRequest request)
